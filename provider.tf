2a1d5b6262b771cd5b5735df4c26dc9fe2d01617
ruediger maass <ruediger.maass@de.ibm.com>
2017-05-18 01:04:20 +0200

    add sl timeout

--- provider.tf
+++ provider.tf
@@ -2,4 +2,5 @@ provider "ibmcloud" {
   skip_service_configuration = ["bluemix"]
   softlayer_api_key  = "${var.ibmcloud_sl_username}"
   softlayer_username = "${var.ibmcloud_sl_api_key}"
+  softlayer_timeout  = 300
 }
