2c0131381dea7a2b8f38f85ae7480c08b510cbe9
ruediger maass <ruediger.maass@de.ibm.com>
2017-06-14 13:18:46 +0200

    Create vm.tf

--- vm.tf
+++ vm.tf
@@ -14,3 +14,17 @@ resource "ibmcloud_infra_virtual_guest" "debian_small_virtual_guest" {
   dedicated_acct_host_only = true
   local_disk               = false
 }
+resource "ibmcloud_infra_virtual_guest" "debian_small_virtual_guest2" {
+  os_reference_code        = "DEBIAN_7_64"
+  domain                   = "cam.ibm.com"
+  datacenter               = "wdc04"
+  network_speed            = 100
+  hourly_billing           = true
+  private_network_only     = false
+  cores                    = 1
+  memory                   = 1024
+  disks                    = [25, 10, 20]
+  user_metadata            = "{\"value\":\"2\"}"
+  dedicated_acct_host_only = true
+  local_disk               = false
+}
