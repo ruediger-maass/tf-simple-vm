7ea4d63616a86d215d432105fd84128343cdb630
ruediger maass <ruediger.maass@de.ibm.com>
2017-06-26 14:20:01 +0200

    simplify names

--- variables.tf
+++ variables.tf
@@ -1,16 +1,10 @@
-variable "ibmcloud_bmx_api_key" {
-   type = "string"
-   default = ""
-   description = "Your IBM Bluemix API key."
-}
-
-variable "ibmcloud_sl_username" {
+variable "sl_username" {
    type = "string"
    default = ""
    description = "Your Softlayer username."
 }
 
-variable "ibmcloud_sl_api_key" {
+variable "sl_api_key" {
    type = "string"
    default = ""
    description = "Your Softlayer API key."
